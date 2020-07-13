f6494b469984666712b35c0e1b1fd89f0bd008d0
Aman Upadhyay <55270177+xBadshahx@users.noreply.github.com>
2020-07-13 17:14:52 +0530

    Update append.cpp

--- Solutions/append.cpp
+++ Solutions/append.cpp
@@ -1,11 +1,11 @@
-/*
-
-Author @Badshah Aman
-
-*/
 #include <iostream>
 using namespace std;
-
+/*
+ * 
+ * Aman Upadhyay
+ * Amity University Kolkata
+ *  
+*/
 int main(){
 string fristName = "Ye Koi ";
 string secondName ="Randikhana Hai";
