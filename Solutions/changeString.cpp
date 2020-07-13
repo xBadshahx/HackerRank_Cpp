c628a1507bbdbc1a79382617a2e4349e4806700c
Aman Upadhyay <55270177+xBadshahx@users.noreply.github.com>
2020-07-13 17:16:00 +0530

    Update changeString.cpp

--- Solutions/changeString.cpp
+++ Solutions/changeString.cpp
@@ -1,12 +1,12 @@
-/*
-
-Author @Badshah Aman
-
-*/
 #include <iostream>
 #include <string>
 using namespace std;
-
+/*
+ * 
+ * Aman Upadhyay
+ * Amity University Kolkata
+ *  
+*/
 int main(){
     string myString = "Hello!";
     myString[0] = 'L';
